572374a7a1668a9b165a5fd145893c044797c7fe
Russel Kurt Nolasco <kurtlonolasco@gmail.com>
2024-02-23 20:38:30 +0800

    material tailwind added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import Home from "../components/Home/Home"
+import { Button } from "@material-tailwind/react";
 
 const HomePage = () => {
   return (
     <>
         <Home />
+        <Button color="white">Button</Button>
     </>
   )
 }
